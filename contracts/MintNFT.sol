62be2ab7499962fd0669128128770912602006b1
kimeunju <lov.ej.eg.ms@gmail.com>
2023-12-06 16:59:01 +0900

    first commit

---
+++ contracts/MintNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract MintNFT is ERC721Enumerable {
+    mapping(uint => string) metadataURIs;
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    function mintNFT(string memory _metadataURI) public {
+        uint tokenId = totalSupply() + 1;
+
+        _mint(msg.sender, tokenId);
+
+        metadataURIs[tokenId] = _metadataURI;
+    }
+
+    function tokenURI(uint _tokenId) public view override returns(string memory) {
+        return metadataURIs[_tokenId];
+    }
+}
